e8c184acac7f3760834210aec00f56ec78636043
Richard-Mro <mololuwa.olorunsola@gmail.com>
2023-09-05 13:46:50 +0100

    Initial commit

---
+++ src/StarRating.jsx
@@ -0,0 +1,12 @@
+function StarRating({ rating }) {
+  const stars = Array.from({ length: rating }, (_, index) => (
+    <img
+      alt=""
+      className="gold-star"
+      src="https://sandpack-bundler.vercel.app/img/gold-star.svg"
+    />
+  ));
+  return <div className="star-wrapper">{stars}</div>;
+}
+
+export default StarRating;
